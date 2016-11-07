5ac85e1d43e5d1a581e3556ee3a2faa1434bf359
Patrick Mithamo <patrickmithamo@gmail.com>
2016-11-07 15:07:10 +0300

    Home page update.
    Add csrf_expemt.
    Return something, always.

--- views.py
+++ views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect, HttpResponseNotFound, HttpResponseBadRequest
 from django.shortcuts import render
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
 from random import choice
 from forms import BasicRuleForm
 from models import BasicRule
-
-
 from envaya.forms import *
 
+@csrf_exempt
 def home(request):
     """root page for envaya : where all the rules are processed"""
 
@@ -16,8 +16,8 @@ def home(request):
             try:
                 rule_to_apply = BasicRule.objects.get(in_text=request.POST["message"])
                 return render(request, 'envaya/index.json' , {'message':rule_to_apply.out_text, 'to':request.POST["from"]})
-            except:
-                pass
+            except Exception as e:
+                return render(request, 'envaya/index.json',{'message':'Error:%s' % (e)})
     else:
         return render(request, 'envaya/index.json',{'message':'Envaya'})
 
